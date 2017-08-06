15a7adf9632331a0730c26cf31d9440d9501896e
Jeff Cleverley <jeff@jeffcleverley.com>
2017-08-06 16:50:28 +0800

    Config archive template helpers.php to use configs to multiple CPTs

--- src/faq/templates/helpers.php
+++ src/faq/templates/helpers.php
@@ -10,10 +10,14 @@
  *
  */
 namespace  Deftly\Module\FAQ\Templates;
+use Deftly\Module\FAQ\Custom;
 
 add_filter( 'archive_template', __NAMESPACE__ . '\load_the_faq_archive_template' );
 /**
- * Load the new Custom Post Type archives from the theme.
+ * Load the new Custom Post Type archives from the plugin.
+ * Use configs from Custom\custom_post_type_configs().
+ *
+ * REMEMBER to make /arhive-{slug}.php templates for each CPT.
  *
  * @since   0.0.1
  *
@@ -22,19 +26,22 @@ add_filter( 'archive_template', __NAMESPACE__ . '\load_the_faq_archive_template'
  * @return  string
  */
 function load_the_faq_archive_template( $archive_template ) {
-	if ( ! is_post_type_archive( 'faq' ) ) {
+	$cpt_configs = Custom\custom_post_type_configs();
+	if ( ! $cpt_configs ) {
 		return $archive_template;
 	}
 
-	$plugin_archive_template = __DIR__ . '/archive-faq.php';
+	foreach ( $cpt_configs as $cpt_config ) {
+		if ( ! is_post_type_archive( $cpt_config['slug'] ) ) {
+			return $archive_template;
+		}
 
-	if ( ! $archive_template ) {
-		return $plugin_archive_template;
+		$plugin_archive_template = __DIR__ . "/archive-{$cpt_config['slug']}.php";
+		$is_in_archive_template = strpos( $archive_template, "/archive-{$cpt_config['slug']}.php");
+		if ( ! $archive_template || ! $is_in_archive_template ) {
+			return $plugin_archive_template;
+		}
+
+		return $archive_template;
 	}
-
-	if ( strpos( $archive_template, '/_archive-faq.php') === false ) {
-		return $plugin_archive_template;
-	}
-
-	return $archive_template;
 }
