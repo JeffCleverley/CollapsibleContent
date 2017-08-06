5d79e8f42494bea9c45237e57f3ce85ae8c64cc0
Jeff Cleverley <jeff@jeffcleverley.com>
2017-08-06 16:18:45 +0800

    Allow theme archive template  with matching cpt slug  to override plugin archive template

--- src/faq/templates/helpers.php
+++ src/faq/templates/helpers.php
@@ -12,10 +12,29 @@
 namespace  Deftly\Module\FAQ\Templates;
 
 add_filter( 'archive_template', __NAMESPACE__ . '\load_the_faq_archive_template' );
+/**
+ * Load the new Custom Post Type archives from the theme.
+ *
+ * @since   0.0.1
+ *
+ * @param   string   $archive_template
+ *
+ * @return  string
+ */
 function load_the_faq_archive_template( $archive_template ) {
 	if ( ! is_post_type_archive( 'faq' ) ) {
 		return $archive_template;
 	}
 
-	return __DIR__ . '/archive-faq.php';
+	$plugin_archive_template = __DIR__ . '/archive-faq.php';
+
+	if ( ! $archive_template ) {
+		return $plugin_archive_template;
+	}
+
+	if ( strpos( $archive_template, '/_archive-faq.php') === false ) {
+		return $plugin_archive_template;
+	}
+
+	return $archive_template;
 }
