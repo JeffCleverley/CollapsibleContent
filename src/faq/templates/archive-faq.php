29da31cb6b5a161ba30d06d77d06cb44ecc4f0a1
Jeff Cleverley <jeff@jeffcleverley.com>
2017-08-06 21:28:46 +0800

    Faq archive views

--- src/faq/templates/archive-faq.php
+++ src/faq/templates/archive-faq.php
@@ -11,6 +11,8 @@
  */
 namespace  Deftly\Module\FAQ\Templates;
 
+use function Composer\Autoload\includeFile;
+
 remove_action( 'genesis_loop', 'genesis_do_loop' );
 add_action( 'genesis_loop', __NAMESPACE__ . '\do_faq_archive_loop');
 /**
@@ -27,8 +29,20 @@ function do_faq_archive_loop() {
 	}
 
 	foreach ( $records as $key => $record ) {
-		//Load the view file
-		d( $record );
+		include( __DIR__ . '/views/container.php' );
+	}
+}
+
+function loop_and_render_faqs( array $faqs ) {
+	$attributes = array(
+		'show_icon' =>  'dashicons dashicons-arrow-down-alt2 show-icon',
+		'hide_icon' =>  'dashicons dashicons-arrow-up-alt2 hide-icon',
+	);
+
+	foreach ( $faqs as $faq ) {
+		$hidden_content =  do_shortcode( $faq['post_content'] );
+
+		include( __DIR__ . '/views/faq.php' );
 	}
 }
 
